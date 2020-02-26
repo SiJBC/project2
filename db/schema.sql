53af154b977d62274dec3afbbaa72e299be11ad3
James Pearce <jamesquintanapearce@gmail.com>
2020-02-26 21:31:58 +1100

    schema updated

--- db/schema.sql
+++ db/schema.sql
@@ -1,27 +1,38 @@
-DROP DATABASE IF EXISTS one_page_manDB;
-CREATE database one_page_manDB;
+DROP DATABASE IF EXISTS one_page_man_db;
+CREATE database one_page_man_db;
 
-USE one_page_manDB;
+USE one_page_man_db;
 
-CREATE TABLE customer (
+CREATE TABLE user_table (
   user_id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  title VARCHAR(70),
-  header_image BLOB,
-  block_1_image BLOB,
-  block_1_text TEXT,
-  block_2_image BLOB,
-  block_2_text TEXT,
-  block_3_image BLOB,
-  block_3_text TEXT, 
   PRIMARY KEY (user_id)
 );
 
-CREATE TABLE user_contact (
-  FOREIGN KEY (user_id) REFERENCES customer(user_id),
+CREATE TABLE page_table (
+  page_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES user_table(user_id),
+  title VARCHAR(70),
+  tagline VARCHAR(265),
+  header_image BLOB,
+  block_1_image BLOB,
+  block_1_head VARCHAR(70),
+  block_1_text TEXT,
+  block_2_image BLOB,
+  block_2_head VARCHAR(70),
+  block_2_text TEXT,
+  block_3_image BLOB,
+  block_3_head VARCHAR(70),
+  block_3_text TEXT, 
   e_mail VARCHAR(100),
   phone VARCHAR(15),
   place_location VARCHAR(100),
-)
+  PRIMARY KEY (page_id)
+);
+
+
+
+
 
