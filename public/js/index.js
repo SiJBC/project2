6ab457d79f027d3bf4be5952042c63a3bb20d3d5
Sherry <a-sherry@live.com>
2020-03-03 23:00:01 +1100

    User form template and select theme function in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var checker = false;
 
 
-    // login section and user choice section
+    //click to show login section and user choice section
     function userLogin() {
         $("#login").click(function () {
             event.preventDefault();
@@ -21,6 +21,7 @@ $(document).ready(function () {
     }
     userLogin();
 
+    //user select what to add into website
     function userChoices() {
         $(".btnList").click(function () {
             event.preventDefault();
@@ -70,6 +71,7 @@ $(document).ready(function () {
     }
     userChoices();
 
+    //user choose title/tagline/description return input or textarea
     function createList(labelFor, textFor) {
         var userForm = $("#userConfirmTable");
         var createLi = $("<li>").addClass("col m12 userConfirmList");
@@ -78,6 +80,7 @@ $(document).ready(function () {
         var createBtn = $("<button>").addClass("col s2 waves-effect waves-light btn btnDelete");
         var createMinus = $("<i>").addClass("fa fa-minus");
 
+        //loop to create id
         for (var c = 0; c < createLi.length; c++) {
             var idNum = c;
         }
@@ -108,6 +111,7 @@ $(document).ready(function () {
         removeDiv(btnRemove);
     };
 
+    //user choose to add product/service or contact details
     function proSevContact(labelFor) {
         //create input and textarea
         var userForm = $("#contactDetails");
@@ -136,6 +140,7 @@ $(document).ready(function () {
             createLi.append(createBtnOne);
             createLi.append(createBtnTwo);
 
+            //new div created append into the existing div in html #contactDetails
             $("#contactDetails").click(addContact);
 
             function addContact() {
@@ -185,7 +190,8 @@ $(document).ready(function () {
             createBtnThree.append(iconPlus);
             createLi.append(createBtnThree);
             createLi.append(createBtnTwo);
-
+            
+            //new div created append into the existing div in html #contactDetails
             $("#contactDetails").click(addContact);
 
             function addContact() {
@@ -257,19 +263,35 @@ $(document).ready(function () {
             var imgId = $(this).attr("id");
             console.log(imgId);
 
-            
+            if (imgId == "mountain") {
+                var image = "#mountain";
+                show(image);
+            } else if (imgId == "sunset") {
+                var image = "#sunset";
+                show(image);
+            } else if (imgId == "forest") {
+                var image = "#forest";
+                show(image);
+            };
             // var label = $(this).parent().siblings().attr("id");
             // console.log(label);
         })
     }
     selectPic();
 
+    //image opacity change to 1 when click, and other image opacity change to 0.5
+    function show(image) {
+        // console.log(image);
+        $(image).removeClass();
+        $(image).addClass("col s4 imageChose");
+        $(image).siblings().removeClass();
+        $(image).siblings().addClass("col s4 images");
+    }
     // function updateCountdown() {
     //     // 140 is the max message length
     //     var remaining = 140 - jQuery('.message').val().length;
     //     jQuery('.countdown').text(remaining + ' characters remaining.');
     // }
-
 
     // ** function to deal with the hide and show
 });
